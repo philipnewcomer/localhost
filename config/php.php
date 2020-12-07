ab420fd39c4d60e2c63f38d0ac0fa10514b02592
Philip Newcomer <contact@philipnewcomer.net>
2020-12-07 17:37:09 -0500

    Add support for PHP 8.0

--- config/php.php
+++ config/php.php
@@ -3,7 +3,7 @@
 return [
     'config_directory' => '/usr/local/etc/php/{version}/conf.d',
     'fpm_config_directory' => '/usr/local/etc/php/{version}/php-fpm.d',
-    'default_version' => '7.4',
+    'default_version' => '8.0',
     'socket_directory' => sprintf(
         '%s/.%s',
         config('environment.user_home_directory_path'),
@@ -12,7 +12,8 @@ return [
     'versions' => [
         '7.2',
         '7.3',
-        '7.4'
+        '7.4',
+        '8.0'
     ],
     'openssl_cert_path' => '/usr/local/etc/openssl@1.1/cert.pem'
 ];
