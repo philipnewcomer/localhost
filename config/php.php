c9e0d0c18e50ae20fd4c5a6bf85a02e8d76c980a
Philip Newcomer <contact@philipnewcomer.net>
2024-03-16 19:08:31 -0400

    Add support for PHP 8.3

--- config/php.php
+++ config/php.php
@@ -2,7 +2,7 @@
 
 return [
     'config_directory' => getenv('HOMEBREW_PREFIX') . '/etc/php/{version}',
-    'default_version' => '8.2',
+    'default_version' => '8.3',
     'socket_directory' => sprintf(
         '%s/.%s',
         config('environment.user_home_directory_path'),
@@ -10,7 +10,8 @@ return [
     ),
     'versions' => [
         '8.1',
-        '8.2'
+        '8.2',
+        '8.3'
     ],
     'openssl_cert_path' => '/usr/local/etc/openssl@1.1/cert.pem'
 ];
